05ffa75347aa35436f01a4a413caeb0e29a12134
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-08-24 10:14:02 -0600

    .

--- deliverFood/settings.py
+++ deliverFood/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if os.getcwd() == '/apps':
+if os.getcwd() == '/app':
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
