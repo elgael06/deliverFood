15ad31572bfcbd2d6f25da2a678d8deeeaa07b9f
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-08-25 20:00:02 -0600

    correccion id.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom';
 
 import Table from '@material-ui/core/Table';
 import { TableHead, TableRow, TableCell, TableBody, Button, TableFooter, TablePagination, Fab, IconButton, MenuList, MenuItem } from '@material-ui/core';
-import {Add, Edit, Delete} from '@material-ui/icons';
+import {Add, Edit, Delete, ImageRounded } from '@material-ui/icons';
 
 const HomeApp =()=>{
     const {list=[],page=1} = useSelector(state=>state.productsStore);
@@ -28,6 +28,7 @@ const HomeApp =()=>{
            <TableHead>
                <TableRow >
                    <TableCell> # </TableCell>
+                   <TableCell>Image</TableCell>
                    <TableCell> NOMBRE </TableCell>
                    <TableCell> COSTO </TableCell>
                    <TableCell> PRECIO </TableCell>
@@ -36,8 +37,13 @@ const HomeApp =()=>{
            </TableHead>
            <TableBody>
                { list.map(e=>{
+                   let id = e.pk.toString();
+                   while(id.length<6){
+                       id = '0'+ id;
+                   }
                    return(<TableRow key={e.pk}>
-                       <TableCell>{e.pk}</TableCell>
+                       <TableCell>{id}</TableCell>
+                       <TableCell> { e.image ? <img style={{height:45,width:45,borderRadius:100}} src={e.image} /> : <ImageRounded fontSize='45px' /> }</TableCell>
                        <TableCell>{e.nombre}</TableCell>
                        <TableCell>{e.costo}</TableCell>
                        <TableCell>{e.price}</TableCell>
