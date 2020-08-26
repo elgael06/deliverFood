17cefc423201d3cd1ea6f056091ff2caf388471b
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-08-25 20:14:46 -0600

    a..

--- frontend/src/actions/products.js
+++ frontend/src/actions/products.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchProductos = () =>{
 
     return async dispatch =>{
-        const res = await axios.get('http://localhost:8000/api/store/products/');
+        const res = await axios.get('https://rocky-harbor-12898.herokuapp.com/api/store/products/');
         console.log(res.data);
         if(res.data){
             dispatch({type:'ADD_PRODUCTS',value:res.data.data,page:res.data.data.numpages})
