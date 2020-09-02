e015685f98e382820dc960de8441a89c7d06587a
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-01 20:54:22 -0600

    error peticion.

--- frontend/src/actions/products.js
+++ frontend/src/actions/products.js
@@ -6,10 +6,14 @@ const url = process.env['NODE_ENV']==='development' ? api.dev : api.prod;
 export const fetchProductos = () =>{
     return async dispatch =>{
         dispatch({type:'LOADING',value:true});
-        const res = await allProductos();
-        console.log(res);
-        if(res){
-            dispatch({type:'ADD_PRODUCTS',value:res.data,page:res.data.numpages})
+        try{
+            const res = await allProductos();
+            console.log(res);
+            if(res){
+                dispatch({type:'ADD_PRODUCTS',value:res.data,page:res.data.numpages})
+            }
+        }catch(e){
+            alert('Error al cargar ',e.toString())
         }
         dispatch({type:'LOADING',value:false});
     }
@@ -18,13 +22,17 @@ export const fetchProductos = () =>{
 export const postProduct = (data=null) =>{
     return async dispatch =>{
         dispatch({type:'LOADING',value:true});
-        const res = await addProducto({data});
-        console.log(res);
-        if(res){
-            const prods = await allProductos();
-            console.log(prods);
-            dispatch({type:'ADD_PRODUCTS',value:prods.data,page:prods.data.numpages});
-            window.location.href = '/ProductsList'
+        try{
+            const res = await addProducto({data});
+            console.log(res);
+            if(res){
+                const prods = await allProductos();
+                console.log(prods);
+                dispatch({type:'ADD_PRODUCTS',value:prods.data,page:prods.data.numpages});
+                window.history.back();
+            }
+        }catch(err){
+            alert('error al guardar producto',err.toString());
         }
         dispatch({type:'LOADING',value:false});
     }
@@ -33,11 +41,16 @@ export const postProduct = (data=null) =>{
 export const getProductId = id =>{
     return async dispatch =>{
         dispatch({type:'LOADING',value:true});
-        dispatch({type:'SELECT_PRODUCT',value:null});
-        const value = await idProducto(id);
-        console.log(value);
-        if(value){
-            dispatch({type:'SELECT_PRODUCT',value});
+        try{
+            dispatch({type:'SELECT_PRODUCT',value:null});
+            const value = await idProducto(id);
+            console.log(value);
+            if(value){
+                dispatch({type:'SELECT_PRODUCT',value});
+            }
+        }catch(err){
+            alert(`el producto con id #${id} no se encontro`,err.toString());
+            window.history.back();
         }
         dispatch({type:'LOADING',value:false});
     }
@@ -46,12 +59,16 @@ export const getProductId = id =>{
 export const putProductId = (data=null) =>{
     return async dispatch =>{
         dispatch({type:'LOADING',value:true});
-        const res = await updateProducto({data});
-        console.log(res);
-        if(res){
-            const prods = await allProductos();
-            dispatch({type:'ADD_PRODUCTS',value:prods.data,page:prods.data.numpages});
-            window.location.href = '/ProductsList'
+        try{
+            const res = await updateProducto({data});
+            console.log(res);
+            if(res){
+                const prods = await allProductos();
+                dispatch({type:'ADD_PRODUCTS',value:prods.data,page:prods.data.numpages});
+                window.history.back();
+            }
+        }catch(err){
+            alert(`error al actualizar el producto!`);
         }
         dispatch({type:'LOADING',value:false});
     }
@@ -60,11 +77,17 @@ export const putProductId = (data=null) =>{
 export const deleteProductId = (id=0) =>{
     return async dispatch =>{
         dispatch({type:'LOADING',value:true});
-        const res = await deleteProducto({id});
-        dispatch({type:'ADD_PRODUCTS',value:[],page:0});
-        
-        const prods = await allProductos();
-        dispatch({type:'ADD_PRODUCTS',value:prods.data,page:prods.data.numpages});
+        try{
+            await deleteProducto({id});
+            dispatch({type:'ADD_PRODUCTS',value:[],page:0});
+            
+            const prods = await allProductos();
+            dispatch({type:'ADD_PRODUCTS',value:prods.data,page:prods.data.numpages});
+            
+        }catch(err){
+            alert(`error al eliminar el producto id ${id}`);
+        }
+
         dispatch({type:'LOADING',value:false});
     }
 }
