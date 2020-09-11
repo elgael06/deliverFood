03d6db475f74704aa35f3fa9b79f4775ee035591
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-10 20:04:41 -0600

    correcion build.

--- config/settings.py
+++ config/settings.py
@@ -1,21 +1,10 @@
 
 from pathlib import Path
 import os
-import django_heroku
-# import mimetypes
+# import django_heroku
 
-# mimetypes.guess_all_extensions(type="text/html",strict=False)
-# mimetypes.guess_type(strict=False)
-# mimetypes.add_type("text/css", ".css", True)
-# mimetypes.add_type("text/html", ".css", True)
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# TPL_DIR = os.path.join(BASE_DIR, 'templates/')
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'build')
-# SITE_ROOT = os.path.join(BASE_DIR, 'build', 'static')
-
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'x1mul9xsa1srttw7h7%rewq%=j$+$@!fs0xioyey&f4=o8clie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -32,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'rest_framework',
     # 'create_react_app',
@@ -44,7 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -60,8 +49,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # os.path.join(BASE_DIR, 'templates'),
-            # TPL_DIR,
             os.path.join(BASE_DIR, 'build'),
         ],
         'APP_DIRS': True,
@@ -103,10 +90,6 @@ DATABASES = {
 #         }
 #     }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -137,21 +120,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-# STATIC_ROOT = '/frontend/public/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'build/static/'),
+    os.path.join(BASE_DIR, 'build/static/'),
 ]
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-# Configure app for Heroku deployment
-django_heroku.settings(locals())
-
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
