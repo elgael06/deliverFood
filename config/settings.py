8fa1a5ba43cf6c4411182746bee663c03d6124e8
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-11 21:46:47 -0600

    template.

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'template'),
+          TPL_DIR
         ],
         'APP_DIRS': True,
         'OPTIONS': {
